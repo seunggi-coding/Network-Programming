d64d46a03e5822c2409628f1a1c6680662def7da
Moon Seung Gi <62280684+seunggi-coding@users.noreply.github.com>
2024-04-30 10:13:02 +0900

    Delete mid_term_exam directory

--- mid_term_exam/p17.py
+++
@@ -1,9 +0,0 @@
-mylist = []
-
-mylist = input("Enter numbers separated by spaces: ").split(" ")
-
-for i in range(len(mylist)):
-    mylist[i] = int(mylist[i])
-    
-result = map(lambda x: x**2, mylist)
-print(f"Squared numbers: {list(result)}")
